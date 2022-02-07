6d7d1f180be1cf2051d77202f3136b24ccc88029
marieAnge <mpoumboura@csss.es>
2022-02-07 17:18:52 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+const name = document.getElementById('name')
+const email = document.getElementById('email')
+const phone = document.getElementById('phone')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+// form.addEventListener('submit', (e) => {
+//     let messages = []
+//     e.preventDefault()
+//     if (name.value === ''|| name.value== null ) {
+//         console.log("name is required")
+//         messages.push('name is required')
+//     }
+
+//     if (messages.length > 0){
+//         e.preventDefault()
+//         errorElement.innerText = messages.join(', ')
+//     }
+// })
